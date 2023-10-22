cc1f6ae985c2015cdf9fad3a0e1ff077f50db6a1
Jakob Miksch <info@jakobmiksch.eu>
2023-10-22 21:35:45 +0200

    Basic map

--- src/views/MapA.vue
+++ src/views/MapA.vue
@@ -2,8 +2,42 @@
   <div>
     <h1>map A</h1>
   </div>
+  <div id="openlayers-map"/>
 </template>
 
+<script setup lang="ts">
+
+import { onMounted} from 'vue'
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+onMounted(()=>{
+
+  const map = new Map({
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    target: 'openlayers-map',
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+  });
+
+})
+
+</script>
+
 <style>
+
+
+#openlayers-map {
+  width: 500px;
+  height: 500px;
+}
 
 </style>
