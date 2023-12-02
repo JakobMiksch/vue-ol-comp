8e5a9a637b29bd9e9966338520eea486c04b316b
Jakob Miksch <jakob.miksch@siticom.de>
2023-12-02 13:50:57 +0100

    vite lib mode

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import olMap from "./components/olMap.vue";
+import { useOl } from "./composables/useOl";
+
+export {olMap, useOl}
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "vue-ol-composable",
+      fileName: "vue-ol-composable",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      // TODO: add OpenLayers
+      external: ["vue", "vue-router"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
