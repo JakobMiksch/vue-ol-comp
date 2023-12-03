8c754efcf2b75d34a7a3eb431ccf0000a1f41ac0
Jakob Miksch <jakob.miksch@siticom.de>
2023-12-03 19:11:54 +0100

    add ts types

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    dts({ include: ['src'] })
   ],
   build: {
     copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "vue-ol-composable",
-      fileName: "vue-ol-composable",
+      formats: ['es']
+      // name: "vue-ol-composable",
+      // fileName: "vue-ol-composable",
     },
     rollupOptions: {
       external: ["vue", "vue-router", "ol"],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: "Vue",
         },
