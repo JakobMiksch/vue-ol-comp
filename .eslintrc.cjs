7319138e7013fb53e7be79c75c468c927fd5d3ee
Jakob Miksch <jakob.miksch@siticom.de>
2023-12-07 22:05:37 +0100

    eslint import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,14 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  plugins: ['sonarjs'],
+  plugins: ['sonarjs', "import"],
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier',
-    'plugin:sonarjs/recommended'
+    'plugin:sonarjs/recommended',
+    "plugin:import/recommended",
   ],
   parserOptions: {
     ecmaVersion: 'latest'
