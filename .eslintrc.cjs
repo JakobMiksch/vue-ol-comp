ec065d81a3b2f2cd78e94d9113d547c567003bb0
Jakob Miksch <jakob.miksch@siticom.de>
2023-12-07 22:03:23 +0100

    add sonar js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,13 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  plugins: ['sonarjs'],
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-prettier',
+    'plugin:sonarjs/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
